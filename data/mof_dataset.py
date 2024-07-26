9bab3fc8ee73352dbb67c4faf52f678cacc03969
NaYoungKim <lkny123@kaist.ac.kr>
2024-07-26 16:32:25 +0900

    use newly defined equivariant axes function f(X)

--- data/mof_dataset.py
+++ data/mof_dataset.py
@@ -45,7 +45,7 @@ class MOFDataset(Dataset):
                 bb.lengths,
                 bb.angles,
                 bb.num_atoms
-            ).numpy()
+            ).numpy().astype(np.float64)
             return cart_coords
 
         bb_pos = []
@@ -71,14 +71,81 @@ class MOFDataset(Dataset):
         return gt_coords, gt_trans, local_coords
 
     @staticmethod
-    def _rotate_bb(bb_coord, bb_atom_type):
+    def _get_equiv_vec(cart_coords, atom_types):
+
+        centroid = np.mean(cart_coords, axis=0)
+
+        # Center of mass weighted by atomic number
+        weight = atom_types / atom_types.sum()
+        weighted_centroid = np.sum(cart_coords * weight[:, None], axis=0)
+
+        # Equivariant vector
+        equiv_vec = weighted_centroid - centroid
+
+        # If v = 0 (symmetric), take the closest non-zero atom
+        if np.allclose(equiv_vec, 0):
+            dist = np.linalg.norm(cart_coords, axis=1)
+            sorted_indices = np.argsort(dist)
+
+            i = 0
+            while i < len(sorted_indices) and np.allclose(equiv_vec, 0):
+                equiv_vec = cart_coords[sorted_indices[i]]
+                i += 1
+        
+        assert not np.allclose(equiv_vec, 0), "Equivariant vector is zero"
+        return equiv_vec
+    
+    @staticmethod
+    def _get_pca_axes(data):
+        # Center the data
+        data_mean = np.mean(data, axis=0)
+        centered_data = data - data_mean
+
+        # Compute the covariance matrix
+        covariance_matrix = np.cov(centered_data, rowvar=False)
+        if covariance_matrix.ndim == 0:
+            return np.zeros(3), np.eye(3)
+
+        # Compute eigenvalues and eigenvectors
+        eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
+
+        # Sort eigenvalues and eigenvectors in descending order
+        sorted_indices = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[sorted_indices]
+        eigenvectors = eigenvectors[:, sorted_indices]
+
+        return eigenvalues, eigenvectors    
+
+    def _get_equivariant_axes(self, cart_coords, atom_types):
+        """
+        Return:
+            R: equivariant rotation matrix
+        """
+
+        if cart_coords.shape[0] == 1:
+            return np.eye(3)
+
+        equiv_vec = self._get_equiv_vec(cart_coords, atom_types)    # v(X)
+
+        _, axes = self._get_pca_axes(cart_coords)                   # PCA(X)
+        ve = equiv_vec @ axes
+        flips = ve < 0 
+        axes = np.where(flips[None], -axes, axes)
+
+        right_hand = np.stack(
+            [axes[:, 0], axes[:, 1], np.cross(axes[:, 0], axes[:, 1])], axis=1
+        )
+        
+        return right_hand
+    
+    def _rotate_bb(self, bb_coord, bb_atom_type):
         """
         Returns:
             rotmats: numpy array of shape (3, 3)
             local_coord: numpy array of shape (n_bb_atoms, 3) 
         """
-        rotmats = np.eye(3)
-        local_coord = bb_coord
+        rotmats = self._get_equivariant_axes(bb_coord, bb_atom_type) # f(X)
+        local_coord = bb_coord @ rotmats                             # g(X) = X f(X)
 
         return rotmats, local_coord 
 
@@ -110,7 +177,7 @@ class MOFDataset(Dataset):
         local_coords = []
         
         for i, bb_coord in enumerate(centered_bb_coords):
-            bb_atom_type = datapoint.pyg_mols[i].atom_types
+            bb_atom_type = datapoint.pyg_mols[i].atom_types.numpy()
             rotmats, local_coord = self._rotate_bb(bb_coord, bb_atom_type)
             gt_rotmats.append(rotmats)
             local_coords.append(local_coord)
